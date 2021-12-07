8fcddfcc23838928e2803642ce374d65f73f282e
Dragnansia <50181744+Dragnansia@users.noreply.github.com>
2021-12-07 16:29:54 +0100

    add reqwest, more comment and update test

--- src/instance.rs
+++ src/instance.rs
@@ -1,3 +1,32 @@
 /// This is a minecraft instance
 ///
-pub struct Instance {}
+/// # Example
+/// ```
+/// let instance = Instance::default();
+/// // or
+/// let instance = Instance::new("1.18", "/game/dir");
+/// ```
+pub struct Instance {
+    /// This is the name of the folder content
+    /// all minecraft file of this instance
+    pub name: String,
+
+    /// The game dir is a formatted path to all
+    /// minecraft instance download and the current name
+    pub game_dir: String,
+}
+
+impl Default for Instance {
+    fn default() -> Self {
+        Self {
+            name: "Default Instance".to_owned(),
+            game_dir: "".to_owned(),
+        }
+    }
+}
+
+impl Instance {
+    pub fn new(name: String, game_dir: String) -> Self {
+        Self { name, game_dir }
+    }
+}
--- src/user.rs
+++ src/user.rs
@@ -1,8 +1,17 @@
+use reqwest;
+
 #[derive(Debug)]
 pub struct User {
+    /// Email or Pseudo
     pub username: String,
+
+    /// UUID of the current player
     pub uuid: String,
+
+    /// Client token get on connection
     pub client_token: String,
+
+    /// Access token for this account
     pub access_token: String,
 }
 
@@ -28,30 +37,50 @@ impl User {
     }
 }
 
+/// Callback for connection with error or the User data
 type ConnectCallBack = fn(Result<User, &'static str>);
 
 // Send a post request to connect
 // get response and send value on callback or return
 // need to be a async function or used multi threading with
 // callback
+///
+///
+/// # Example
+/// ```
+/// fn cb(Result<User, &'static str>) {}
+///
+/// fn functon() {
+///     try_connect("Username".to_string(), "Password".to_string(), cb);
+///     // or
+///     try_connect("Username".to_string(), "Password".to_string(), |res| {});
+/// }
+/// ```
 pub fn try_connect(username: String, password: String, callback: ConnectCallBack) {
     if username.is_empty() || password.is_empty() {
         callback(Err("Username or password is empty"));
     }
+
+    // Move this on other file
+    let reqwest_client = reqwest::Client::new();
+    let res = reqwest_client
+        .post("https://authserver.mojang.com/authenticate")
+        .body(format!(
+            "{{\"agent\":{{\"name\":\"Minecraft\",\"version\":1}},\"username\": {},\"password\": {} }}",
+            username, password
+        )).send();
 
     callback(Ok(User::default()))
 }
 
 #[cfg(test)]
 mod test {
-    use super::{try_connect, User};
+    use super::try_connect;
 
     #[test]
     fn connect() {
-        try_connect("".to_string(), "".to_string(), result);
-    }
-
-    fn result(res: Result<User, &'static str>) {
-        println!("{:?}", res.unwrap());
+        try_connect("".to_string(), "".to_string(), |res| {
+            println!("{:?}", res.unwrap());
+        });
     }
 }
