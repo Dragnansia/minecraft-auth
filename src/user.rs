24cdb3bf1e6c0a51196f3b03b3d211e2420f53b5
Dragnansia <50181744+Dragnansia@users.noreply.github.com>
2021-12-14 18:06:52 +0100

    update user connection work, add struct to get User

--- src/user.rs
+++ src/user.rs
@@ -1,3 +1,27 @@
+use reqwest::Client;
+use serde_json::Value;
+use tokio::{
+    sync::mpsc::{channel, error::TryRecvError, Receiver},
+    task::JoinHandle,
+};
+
+pub enum UCStatut {
+    User(User),
+    Error(String),
+}
+
+#[derive(Debug)]
+pub struct UConnect {
+    pub receiver: Receiver<UCStatut>,
+    _thread: JoinHandle<()>,
+}
+
+impl UConnect {
+    pub fn message(&mut self) -> Result<UCStatut, TryRecvError> {
+        self.receiver.try_recv()
+    }
+}
+
 #[derive(Debug, PartialEq, PartialOrd, Default)]
 pub struct User {
     /// Email or Pseudo
@@ -22,4 +46,55 @@ impl User {
             access_token,
         }
     }
+}
+
+pub fn connect_to_mojang(username: String, password: String) -> UConnect {
+    let (sender, receiver) = channel(1);
+    let thread = tokio::spawn(async move {
+        let client = Client::new();
+        let body = format!("{{\"agent\": {{\"name\": \"Minecraft\",\"version\":1}},\"username\":\"{}\",\"password\":\"{}\"}}", username, password);
+        let res = client
+            .post("https://authserver.mojang.com/authenticate")
+            .body(body)
+            .send()
+            .await;
+
+        let data: Value = match res {
+            Ok(val) => {
+                let data = val.text().await.unwrap_or_default();
+                serde_json::from_str(&data).unwrap_or_default()
+            }
+            Err(err) => {
+                let _ = sender.send(UCStatut::Error(err.to_string())).await;
+                return;
+            }
+        };
+
+        if let Some(error) = data["errorMessage"].as_str() {
+            let _ = sender.send(UCStatut::Error(error.to_string())).await;
+        } else {
+            let client_token = data["clientToken"].as_str().unwrap().to_string();
+            let access_token = data["accessToken"].as_str().unwrap().to_string();
+            let uuid = data["selectedProfile"]["id"].as_str().unwrap().to_string();
+
+            let _ = sender
+                .send(UCStatut::User(User::new(
+                    username,
+                    uuid,
+                    client_token,
+                    access_token,
+                )))
+                .await;
+        }
+    });
+
+    UConnect {
+        receiver,
+        _thread: thread,
+    }
+}
+
+mod test {
+    #[test]
+    fn mojang_connect() {}
 }
