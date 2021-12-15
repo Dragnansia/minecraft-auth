0056bca1a0cbe227fd760d567b5685ddebffd75b
Dragnansia <50181744+Dragnansia@users.noreply.github.com>
2021-12-15 16:51:55 +0100

    change MinecraftAuth struct to just used a name and a path

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,29 @@
 pub mod downloader;
 pub mod instance;
 pub mod user;
+pub mod version;
 
+#[derive(Clone)]
 pub struct MinecraftAuth {
-    pub app_name: String,
-    pub app_path: String,
-    pub instance_path: String,
-    pub cache_path: String,
+    pub name: String,
+    pub path: String,
 }
 
 impl MinecraftAuth {
-    pub fn new(
-        app_name: String,
-        app_path: String,
-        instance_path: String,
-        cache_path: String,
-    ) -> Self {
-        Self {
-            app_name,
-            app_path,
-            instance_path,
-            cache_path,
+    pub fn new(name: String, path: String) -> Self {
+        Self { name, path }
+    }
+
+    /// Create MinecraftAuth with just a name, and get
+    /// os data dir to create a new folder
+    pub fn new_just_name(name: String) -> Option<Self> {
+        match dirs::data_dir() {
+            Some(d) => {
+                let path = format!("{}/{}", d.to_str().unwrap(), name);
+                std::fs::create_dir_all(path.clone()).unwrap();
+                Some(Self { name, path })
+            }
+            None => None,
         }
     }
 }
