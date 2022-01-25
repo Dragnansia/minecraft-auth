4ee0160196a9f073a1987366f7943bf9f8e93ca0
Dragnansia <50181744+Dragnansia@users.noreply.github.com>
2022-01-25 12:55:00 +0100

    add multiple java path on find

--- src/java.rs
+++ src/java.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 #[cfg(target_os = "linux")]
-const JAVA_PATH: &str = "/usr/lib/jvm/";
+const JAVA_PATH: [&str; 1] = ["/usr/lib/jvm/"];
 #[cfg(target_os = "linux")]
 const JAVA_FOLDER_NAME_B8: &str = "java-1.";
 #[cfg(target_os = "linux")]
@@ -12,7 +12,10 @@ const JAVA_FOLDER_NAME_A8: &str = "java-";
 const JAVA_PATH_END_A8: &str = "bin/java";
 
 #[cfg(target_os = "windows")]
-const JAVA_PATH: &str = "C:\\Program Files\\Java\\";
+const JAVA_PATH: [&str; 2] = [
+    "C:\\Program Files\\Java\\",
+    "C:\\Program Files (x86)\\Java\\",
+];
 #[cfg(target_os = "windows")]
 const JAVA_PATH_END_B8: &str = "bin\\java.exe";
 #[cfg(target_os = "windows")]
@@ -32,20 +35,20 @@ pub fn find_java_version(version: u8) -> Option<String> {
     };
 
     let folder_start = format!("{}{}", folder_java_begin, version);
-    if let Ok(mut dir_content) = fs::read_dir(JAVA_PATH) {
-        if let Some(dir) = dir_content.find(|el| {
-            el.as_ref()
-                .unwrap()
-                .file_name()
-                .to_str()
-                .unwrap()
-                .starts_with(&folder_start)
-        }) {
-            Some(format!("{}/{}", dir.unwrap().path().to_str()?, end_path))
-        } else {
-            None
+    for java_path in JAVA_PATH {
+        if let Ok(mut dir_content) = fs::read_dir(java_path) {
+            if let Some(dir) = dir_content.find(|el| {
+                el.as_ref()
+                    .unwrap()
+                    .file_name()
+                    .to_str()
+                    .unwrap()
+                    .starts_with(&folder_start)
+            }) {
+                return Some(format!("{}/{}", dir.unwrap().path().to_str()?, end_path));
+            }
         }
-    } else {
-        None
     }
+
+    None
 }
